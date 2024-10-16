42164abf1327ea559c0e62061ca89767e634c6c5
tech4m <amitleo81@gmail.com>
2024-10-16 22:43:54 +0530

    Code updated without values

---
+++ EventHandler/HotelCreatedEventHandler.Test/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Amazon.Lambda.SNSEvents;
+using HotelCreatedEventHandler;
+using HotelCreatedEventHandler.Models;
+using System.Text.Json;
+
+Environment.SetEnvironmentVariable("host", "https://507b3c520fed4264a49694ead351d6d4.us-central1.gcp.cloud.es.io");
+Environment.SetEnvironmentVariable("userName", "elastic");
+Environment.SetEnvironmentVariable("password", "UrZfevHAhpsBRH04HMZZnkq5");
+Environment.SetEnvironmentVariable("indexName", "event");
+Environment.SetEnvironmentVariable("AWS_REGION", "us-east-1");
+
+var hotel = new Hotel()
+{
+    Name = "Taj Hotel",
+    City = "Chandigarh",
+    Id = "123",
+    Rating = 4,
+    Price = 100,
+    UserId = "ABC",
+    CreationDateTime = DateTime.Now
+};
+
+var snsEvent = new SNSEvent()
+{
+    Records = new List<SNSEvent.SNSRecord>() { new SNSEvent.SNSRecord() {Sns=new SNSEvent.SNSMessage()
+    {
+        MessageId = "100",
+        Message=JsonSerializer.Serialize(hotel)
+    } } }
+};
+
+
+var handler = new HotelCreatedEventHandler.HotelCreatedEventHandler();
+await  handler.Handler(snsEvent);
---
+++ EventHandler/HotelCreatedEventHandler/HotelCreatedEventHandler.cs
@@ -0,0 +1,51 @@
+﻿using Amazon;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DocumentModel;
+using Amazon.Lambda.Core;
+using Amazon.Lambda.Serialization.SystemTextJson;
+using Amazon.Lambda.SNSEvents;
+using HotelCreatedEventHandler.Models;
+using Nest;
+using System.Text.Json;
+
+[assembly: LambdaSerializer(typeof(DefaultLambdaJsonSerializer))]
+
+namespace HotelCreatedEventHandler;
+public class HotelCreatedEventHandler
+{
+    public async Task Handler(SNSEvent snsEvent)
+    {
+
+
+
+        var connSettings = new ConnectionSettings(new Uri(host));
+        connSettings.BasicAuthentication(userName, password);
+
+        connSettings.DefaultIndex(indexName);
+        connSettings.DefaultMappingFor<Hotel>(m => m.IdProperty(p => p.Id));
+
+        var esClient = new Nest.ElasticClient(connSettings);
+        if (!(await esClient.Indices.ExistsAsync(indexName)).Exists)
+        {
+            await esClient.Indices.CreateAsync(indexName);
+        }
+
+        foreach (var eventRecord in snsEvent.Records)
+        {
+            var eventId = eventRecord.Sns.MessageId;
+            var foundItem = await table.GetItemAsync(eventId);
+            if (foundItem == null)
+            {
+                await table.PutItemAsync(new Document()
+                {
+                    ["eventid"] = eventId
+                });
+
+                //Process Event Here
+            }
+            var hotel = JsonSerializer.Deserialize<Hotel>(eventRecord.Sns.Message);
+
+            await esClient.IndexDocumentAsync<Hotel>(hotel);
+        }
+    }
+}
